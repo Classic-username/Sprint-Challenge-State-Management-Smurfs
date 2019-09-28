762ccc1f80f171619e521d2b9f6536392f7f253d
Joshua Draper <josh.drapergame@gmail.com>
2019-09-27 19:26:26 -0600

    initial save, may have dependencies in wrong parts

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk))
+
+
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById("root"));
