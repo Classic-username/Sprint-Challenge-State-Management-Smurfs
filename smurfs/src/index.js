4cd989f5d65e054853ec1066164de629406a4ab0
Joshua Draper <josh.drapergame@gmail.com>
2019-09-27 19:36:15 -0600

    default state, reducer, store and provider working

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,10 +5,12 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 
+import {reducer} from './reducers'
+
 import App from "./components/App";
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 
 
-ReactDOM.render(<Provider><App /></Provider>, document.getElementById("root"));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
